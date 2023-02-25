ff236cc873bca72ebc4bf397ab56388e4c4d296c
bruno <bpires.siqueira@gmail.com>
2023-02-25 13:13:10 -0300

    ✨ newsletter

--- src/components/Newsletter.jsx
+++ src/components/Newsletter.jsx
@@ -5,13 +5,16 @@ function Newsletter() {
   return (
     <section className="newsletter-bg">
       <div className="news-container">
-        <div>
+        <div className="news-items">
           <img src={carta} alt="Carta icon" />
           <h1>newsletter</h1>
         </div>
         <p>assine e receba todas as novidades e ofertas do marketJson!</p>
       </div>
-      <input type="email" id="email" />
+      <div className="input-box-button">
+        <input type="email" id="email" placeholder="email@email.com" />
+        <input type="button" value="assinar" />
+      </div>
     </section>
   );
 }
