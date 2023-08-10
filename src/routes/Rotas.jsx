045ee7171bd0ec09b7b970b4db4f878db989dc43
bruno <bpires.siqueira@gmail.com>
2023-08-09 23:39:10 -0300

    update

--- src/routes/Rotas.jsx
+++ src/routes/Rotas.jsx
@@ -1,16 +1,16 @@
-import { Routes, Route, BrowserRouter } from "react-router-dom";
-import Home from "../views/pages/Home";
-import Produtos from "../views/pages/Produtos";
-import Contato from "../views/pages/Contato";
-import Sobre from "../views/pages/Sobre";
-import Login from "../views/pages/Login";
-import Admin from "../views/pages/Admin";
+import { Routes, Route, BrowserRouter } from 'react-router-dom';
+import Home from '../views/pages/Home';
+import Produtos from '../views/pages/Produtos';
+import Contato from '../views/pages/Contato';
+import Sobre from '../views/pages/Sobre';
+import Login from '../views/pages/Login';
+import Admin from '../views/pages/Admin';
 
 function Rotas() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<Home />} />
+        <Route path="/Projetos-finais-CASE2" element={<Home />} />
         <Route path="/produtos" element={<Produtos />} />
         <Route path="/contato" element={<Contato />} />
         <Route path="/sobre" element={<Sobre />} />
